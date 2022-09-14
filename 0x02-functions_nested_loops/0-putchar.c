a2271a4edae74053741c66784b3f188e8aeddf3b
Ayoola Ogungbemi <aygbemi@yahoo.co.uk>
2022-09-13 18:19:55 -0700

    prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar.h"
+
+/*
+ *Print _putchar followed by a new line
+ *
+ *
+ */
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
