362731e5c42266c2e125fef4102edb60ad20a193
Ayoola Ogungbemi <aygbemi@yahoo.co.uk>
2022-09-20 18:37:15 -0700

    swaps the values of two integers.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
 /**
- *
- *  * reset_to_98 - update a pointer's referenced value to 98
- *
- *  * @n: pointer reset to 98
+ * reset_to_98 - update a pointer's referenced value to 98
+ * @n: pointer reset to 98
  *  Return:nothing
- *
  */
-
 void reset_to_98(int *n)
-
 {
 
 	*n = 98;
