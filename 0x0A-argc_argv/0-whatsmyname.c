f38c14ae3f857b187927e2dee66b1867ba3ab925
Ayoola Ogungbemi <aygbemi@yahoo.co.uk>
2022-10-05 16:35:11 -0700

    prints the minimum number of coins to make change for an amount of money.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+* main - Entry point
+* @argc: argument count
+* @argv: arguments as array of strings
+* Description: print the name of the program
+* Return: 0 (success)
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+
+}
